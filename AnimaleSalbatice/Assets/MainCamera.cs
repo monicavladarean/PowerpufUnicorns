23d5e36e7c73a49d1a44f83e9652542037d75da1
siminastefan <simina.stefan@yahoo.com>
2020-12-05 15:54:56 +0200

    added puzzle, more to come for checking if it is done

---
+++ AnimaleSalbatice/Assets/MainCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainCamera : MonoBehaviour
+{
+
+    MainCamera mainCamera;
+    Image image1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = GameObject.Find("Main Camera").GetComponent<MainCamera>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
