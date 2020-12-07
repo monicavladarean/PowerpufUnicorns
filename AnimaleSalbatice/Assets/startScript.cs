9b6531e68780e2887e876499b0116b5624c0aa62
monicavladarean <mvladarean@yahoo.com>
2020-12-07 19:18:50 +0200

    Merged activity mame si pui into Prototip

--- AnimaleSalbatice/Assets/startScript.cs
+++ AnimaleSalbatice/Assets/startScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class startScript : MonoBehaviour
 {
@@ -26,6 +27,7 @@ public class startScript : MonoBehaviour
                 if (hit.collider.name == "startButon")
                 {
                     Debug.Log("game starts");
+                    SceneManager.LoadScene("ActivityMamesiPui");
                 }
             }
         }
